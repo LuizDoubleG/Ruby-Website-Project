a2896411438b80769266aa6cd5244c84fa294c34
LightVM <20201100016@pq.uenf.br>
2022-12-27 17:13:53 -0300

    feat: added new content toaula.rb inside 22_12_24 folder

--- 22_12_24/aula.rb
+++ 22_12_24/aula.rb
@@ -1,8 +1,75 @@
-require 'byebug'
-debugger
+# require 'byebug'
+# debugger
 
-h = {"hash" => "yep!"}
+# h = {"hash" => "yep!"}
 
-puts "hello world"
+# puts "hello world"
 
-puts h.class
+# puts h.class
+
+a = 1
+puts a # Normal
+
+@a = 2
+puts @a # Instance
+
+@@a = 3
+puts @@a # Class
+
+$a = 4
+puts $a # Global
+
+A = 5
+puts A # Const
+
+a = 1
+puts a.class
+
+@a = 2
+puts @a.class
+
+# @@a = 3
+puts @@a.class 
+
+$a = 4
+puts $a.class 
+
+# A = 5
+puts A.class 
+
+#
+#
+
+# def teste
+#     puts a 
+# end 
+
+# teste # doesn't work (normal variable used as a instance)
+puts a # works
+
+def teste2
+    puts @a 
+end # instance variable (inside a function)
+
+teste2 #works
+
+
+def teste3
+    puts @@a 
+end
+
+teste3 #works with advice (needs to be inside a class)
+
+
+def teste4
+    puts $a 
+end
+
+teste4 #works 
+
+
+def teste5
+    puts A
+end
+
+teste5 #works 
